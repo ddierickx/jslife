244f4fcc0d86ae41c06ba52a9fbff09a851ce050
dierickx <dierickx@BE1XPL-100239.(none)>
2012-01-26 14:48:16 +0100

    Added raphael to min

--- jslife/scripts/compile-adv.sh
+++ jslife/scripts/compile-adv.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-java -jar /opt/closure-compiler/compiler.jar --compilation_level ADVANCED_OPTIMIZATIONS --js ../src/JSLifeConfiguration.js ../src/JSLifeCore.js --js_output_file jslife-min.js
+java -jar /opt/closure-compiler/compiler.jar --js ../lib/raphael-min.js ../src/JSLifeConfiguration.js ../src/JSLifeCore.js --js_output_file jslife-min.js
